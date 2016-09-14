eff77d31cef6362f5a83efd1fd6cad29707513b6
nicolas7763 <nicolas7763@rambler.ru>
2016-09-14 16:15:25 +0300

    second commit

--- index.php
+++ index.php
@@ -4,14 +4,14 @@
 	<title>Основы php</title>
 </head>
 <body>
-
+<h1>Угадай число от 1 до 100</h1>
 <?php
-if (isset($_GET[submit])) {//если данные переданы
-	$number= $_GET["number"];//пишем данные в переменную из текстового поля формы
-	$compDice= $_GET["compDice"];// пишем в переменную данные из скрытого поля формы
+if (isset($_POST["send"])) {//если данные переданы
+	$number= $_POST["number"];//пишем данные в переменную из текстового поля формы
+	$compDice= $_POST["compDice"];// пишем в переменную данные из скрытого поля формы
 	
 
-if ($number > $_GET["compDice"]) {//проверяем первое условие
+if ($number > $_POST["compDice"]) {//проверяем первое условие
 	echo " Введенное число '$number' больше загаданного числа ";
 }elseif ($number < $compDice) {
 	echo "Введенное число '$number' меньше загаданного числа ";
@@ -25,9 +25,9 @@ if ($number > $_GET["compDice"]) {//проверяем первое услови
 }
 ?>
 
-<form method= "get">
+<form  method= "post">
 Введите число: <input type="text" name="number" value="<?=$number?>" /><br />
-<input type="submit" name="submit" value="Ввод" />
+<input type="submit" name="send" value="Ввод" />
 <input type="hidden" name="compDice" value="<?=$compDice?>">
 </form>
 </body>
